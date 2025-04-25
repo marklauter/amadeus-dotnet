aeb9261a31c214b46c56f765a88be6fad1c28ff8
Mark Lauter <mark@marklauter.com>
2025-04-24 23:17:09 -0400

    added additional logging methods

--- Amadeus.Nine/LoggerExtensions.cs
+++ Amadeus.Nine/LoggerExtensions.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.Extensions.Logging;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Amadeus.Nine;
 
+[SuppressMessage(
+    "Performance",
+    "CA1848:Use the LoggerMessage delegates",
+    Justification = "it's not that big a deal")]
 public static class LoggerExtensions
 {
     public static T Info<T>(
@@ -28,6 +33,90 @@ public static class LoggerExtensions
         params object[] args)
     {
         logger.LogInformation(message, args);
+        return item;
+    }
+
+    public static T Debug<T>(
+        this ILogger logger,
+        T item)
+    {
+        logger.LogDebug("{@Item}", item);
+        return item;
+    }
+
+    public static T Debug<T>(
+        this ILogger logger,
+        T item,
+        string message)
+    {
+        logger.LogDebug(message);
+        return item;
+    }
+
+    public static T Debug<T>(
+        this ILogger logger,
+        T item,
+        string message,
+        params object[] args)
+    {
+        logger.LogDebug(message, args);
+        return item;
+    }
+
+    public static T Warning<T>(
+        this ILogger logger,
+        T item)
+    {
+        logger.LogWarning("{@Item}", item);
+        return item;
+    }
+
+    public static T Warning<T>(
+        this ILogger logger,
+        T item,
+        string message)
+    {
+        logger.LogWarning(message);
+        return item;
+    }
+
+    public static T Warning<T>(
+        this ILogger logger,
+        T item,
+        string message,
+        params object[] args)
+    {
+        logger.LogWarning(message, args);
+        return item;
+    }
+
+    public static T Error<T>(
+        this ILogger logger,
+        Exception ex,
+        T item)
+    {
+        logger.LogError(ex, "{@Item}", item);
+        return item;
+    }
+
+    public static T Error<T>(
+        this ILogger logger,
+        Exception ex,
+        T item,
+        string message)
+    {
+        logger.LogError(ex, message);
+        return item;
+    }
+
+    public static T Error<T>(
+        this ILogger logger,
+        Exception ex,
+        T item,
+        string message,
+        params object[] args)
+    {
+        logger.LogError(ex, message, args);
         return item;
     }
 }
