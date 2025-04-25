f509d44e98f58d59d9d1412ea8b14077a7f09000
Mark Lauter <mark@marklauter.com>
2025-04-24 22:49:44 -0400

    added logger extensions

---
+++ Amadeus.Nine/LoggerExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Amadeus.Nine;
+
+public static class LoggerExtensions
+{
+    public static T Info<T>(
+        this ILogger logger,
+        T item)
+    {
+        logger.LogInformation("{@Item}", item);
+        return item;
+    }
+
+    public static T Info<T>(
+        this ILogger logger,
+        T item,
+        string message)
+    {
+        logger.LogInformation(message);
+        return item;
+    }
+
+    public static T Info<T>(
+        this ILogger logger,
+        T item,
+        string message,
+        params object[] args)
+    {
+        logger.LogInformation(message, args);
+        return item;
+    }
+}
