c3699f20f5ad80011e47038a03d96ac4597e521b
Mark Lauter <mark@marklauter.com>
2025-04-25 22:54:03 -0400

    remove test method

--- Amadeus.Nine/AmadeusClient.cs
+++ Amadeus.Nine/AmadeusClient.cs
@@ -8,9 +8,5 @@ public sealed class AmadeusClient(
     AmadeusOptions options,
     ILogger<AmadeusClient> logger)
 {
-    public async Task PingAsync(CancellationToken cancellationToken)
-    {
-        using var request = new HttpRequestMessage(HttpMethod.Get, "http://microsoft.com");
-        using var response = await httpClient.SendAsync(request, cancellationToken);
-    }
+
 }
