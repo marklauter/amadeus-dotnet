75f34753d890347888e146bf7613b2049aaf91a3
Mark Lauter <mark@marklauter.com>
2025-04-25 22:54:21 -0400

    added tests and test client

---
+++ Amadeus.Nine.Tests/Tokens/AuthTokenVerifier.cs
@@ -0,0 +1,28 @@
+﻿namespace Amadeus.Nine.Tests.Tokens;
+
+public sealed partial class TokensTests
+{
+    internal sealed class AuthTokenVerifier
+        : DelegatingHandler
+    {
+        public bool HasToken { get; private set; }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            HasToken = request.Headers.Authorization is not null;
+            return Task.FromResult(new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            {
+                Content = new StringContent("test response")
+            });
+        }
+
+        protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            HasToken = request.Headers.Authorization is not null;
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            {
+                Content = new StringContent("test response")
+            };
+        }
+    }
+}
---
+++ Amadeus.Nine.Tests/Tokens/TestClient.cs
@@ -0,0 +1,42 @@
+﻿using Amadeus.Nine.Options;
+using Amadeus.Nine.Tokens;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using static Amadeus.Nine.Tests.Tokens.TokensTests;
+
+namespace Amadeus.Nine.Tests.Tokens;
+
+internal sealed class TestClient(HttpClient httpClient)
+{
+    public async Task TestAsync(CancellationToken cancellationToken) =>
+        _ = await httpClient.GetStringAsync("path", cancellationToken);
+
+    internal static IServiceCollection AddTestClient(
+        IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var options = configuration
+            .GetRequiredSection(AmadeusOptions.SectionName)
+            .Get<AmadeusOptions>()
+            ?? throw new InvalidOperationException($"can't read {nameof(AmadeusOptions)} in section {AmadeusOptions.SectionName}");
+
+        var credentials = configuration
+            .GetRequiredSection(nameof(AmadeusCredentials))
+            .Get<AmadeusCredentials>()
+            ?? throw new InvalidOperationException($"can't read {nameof(AmadeusCredentials)} in section {nameof(AmadeusCredentials)}");
+
+        _ = services
+            .AddSingleton(options)
+            .AddSingleton(credentials)
+            .AddTransient<AuthTokenHandler>()
+            .AddSingleton<AuthTokenVerifier>();
+
+        _ = services.AddHttpClient<TokenProvider>(client => client.BaseAddress = options.Host);
+        _ = services.AddHttpClient<TestClient>(client => client.BaseAddress = options.Host)
+            .AddHttpMessageHandler<AuthTokenHandler>()
+            .AddHttpMessageHandler<AuthTokenVerifier>();
+
+        return services;
+    }
+}
+
