8d1e34478a76c2e0516e4a3a6289015a04c7a142
Mark Lauter <mark@marklauter.com>
2025-04-25 23:11:33 -0400

    fixed class structure

--- Amadeus.Nine.Tests/Auth/AuthTokenVerifier.cs
+++ Amadeus.Nine.Tests/Auth/AuthTokenVerifier.cs
@@ -1,28 +1,25 @@
-﻿namespace Amadeus.Nine.Tests.Tokens;
+﻿namespace Amadeus.Nine.Tests.Auth;
 
-public sealed partial class TokensTests
+internal sealed class AuthTokenVerifier
+    : DelegatingHandler
 {
-    internal sealed class AuthTokenVerifier
-        : DelegatingHandler
+    public bool HasToken { get; private set; }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        public bool HasToken { get; private set; }
-
-        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        HasToken = request.Headers.Authorization is not null;
+        return Task.FromResult(new HttpResponseMessage(System.Net.HttpStatusCode.OK)
         {
-            HasToken = request.Headers.Authorization is not null;
-            return Task.FromResult(new HttpResponseMessage(System.Net.HttpStatusCode.OK)
-            {
-                Content = new StringContent("test response")
-            });
-        }
+            Content = new StringContent("test response")
+        });
+    }
 
-        protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
+    protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        HasToken = request.Headers.Authorization is not null;
+        return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
         {
-            HasToken = request.Headers.Authorization is not null;
-            return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
-            {
-                Content = new StringContent("test response")
-            };
-        }
+            Content = new StringContent("test response")
+        };
     }
 }
